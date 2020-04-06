f41b6e9a4d067441d36e11c9a188dfdfe353e194
TIGeMi <maxtrolla1@gmail.com>
2020-04-06 10:01:35 +0700

    Module hóa Manual mode

--- SmartAqua.ino
+++ SmartAqua.ino
@@ -59,30 +59,7 @@ void loop() {
   while(Mode == 1) //che do manual
   {   
     catchEvent(Mode);
-  if(keyTemp == true)//vao lan sau
-        {
-          FirebaseObject object = Firebase.get(path);
-          int fbTemp  = object.getInt("Temp");
-          int currentTemp = getCurrentTemp();
-          delay(1000);
-          Serial.println("Vao che do thay doi nhiet do");
-          Serial.print("Nhiet do hien tai cua ho ca:");
-          delay(500);
-          Serial.println(currentTemp);
-          delay(500);
-          Serial.print("Nhiet do tren Firebase: ");
-          delay(500);
-          Serial.println(fbTemp);
-          delay(500);
-          //kiem tra neu currentTemp = fbTemp thi keyTemp = false
-          if(currentTemp == fbTemp) 
-          {
-            keyTemp = false;
-            // Dong thoi tat so nong lanh
-            Serial.println("Tat so nong lanh...");
-          } else { Serial.println("Chua tat so");}
-          delay(1000);
-        }
+    stopWarmerCooler(keyTemp);
   }
 
 
--- Temp_Module.ino
+++ Temp_Module.ino
@@ -11,5 +11,34 @@ int getCurrentTemp()
 { 
  sensors.requestTemperatures();
  Serial.println(sensors.getTempCByIndex(0));
- return (int)sensors.getTempCByIndex(0);
+ return round (sensors.getTempCByIndex(0));
 } 
+
+void stopWarmerCooler (bool &keyTemp)
+{
+   if(keyTemp == true)//vao lan sau
+  {
+    String path = "/Hoca";
+    FirebaseObject object = Firebase.get(path);
+    int fbTemp  = object.getInt("Temp");
+    int currentTemp = getCurrentTemp();
+    delay(1000);
+    Serial.println("Vao che do thay doi nhiet do");
+    Serial.print("Nhiet do hien tai cua ho ca:");
+    delay(500);
+    Serial.println(currentTemp);
+    delay(500);
+    Serial.print("Nhiet do tren Firebase: ");
+    delay(500);
+    Serial.println(fbTemp);
+    delay(500);
+    //kiem tra neu currentTemp = fbTemp thi keyTemp = false
+    if(currentTemp == fbTemp) 
+    {
+      keyTemp = false;
+      // Dong thoi tat so nong lanh
+      Serial.println("Tat so nong lanh...");
+    } else { Serial.println("Chua tat so");}
+    delay(1000);
+  }
+}
